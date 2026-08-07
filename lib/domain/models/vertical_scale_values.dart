64fe05575c2c836677b54983513e0c89ed539f70
KaueSanchesG <kauesanches123@gmail.com>
2026-08-07 09:42:27 -0300

    fix: adding a mensurable scale to default state of vertical-sacle

--- lib/domain/models/vertical_scale_values.dart
+++ lib/domain/models/vertical_scale_values.dart
@@ -5,9 +5,9 @@ class VerticalScaleValues {
   final double minValue;
 
   const VerticalScaleValues({
-    this.maxValue = 0.0,
+    this.maxValue = 1.0,
     this.minValue = 0.0,
-    this.avgValue = 0.0,
+    this.avgValue = 0.25,
     this.rtValue = 0.0,
   });
 }
